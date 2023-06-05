a7aa558aae6aa8677f70474daef5a5779759357b
PranavModi999 <pranavmodi999@gmail.com>
2023-06-04 20:47:56 -0400

    adding socket to client

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const { Socket } = require("engine.io");
-
 const server = require("http").createServer();
-const io = require("socket.io")(server);
-
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`\x1b[32mlistening on port ${PORT}...\x1b[0m`);
 });
 
-io.on("connection", (Socket) => {
+io.on("connection", (socket) => {
   console.log("a User Connected");
 });
