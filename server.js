3c1b5d327951ce0d1c1f1ec8a3675bc3ac9e764d
PranavModi999 <pranavmodi999@gmail.com>
2023-06-04 21:04:27 -0400

    added refree logic and socket to allow 2 palyers to connect

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ server.listen(PORT, () => {
   console.log(`\x1b[32mlistening on port ${PORT}...\x1b[0m`);
 });
 
+let readyPlayerCount = 0;
+
 io.on("connection", (socket) => {
-  console.log("a User Connected");
+  console.log("a User Connected", socket.id);
+
+  socket.on("ready", () => {
+    console.log("player ready ", socket.id);
+    readyPlayerCount++;
+    if (readyPlayerCount === 2) {
+      console.log("Starting the game");
+      io.emit("startGame", socket.id);
+    }
+  });
 });
